751452a5d65bbcfbb311fd5db1ef5977e9c994dd
rvrajaneesh <54465856+rvrajaneesh@users.noreply.github.com>
2021-07-14 10:16:49 +0530

    Quez 6 Updated

---
+++ Java Assignments/Day3/Quez 6/ChatApp/Message.java
@@ -0,0 +1,21 @@
+package chatApp;
+
+import java.util.Objects;
+
+public class Message {
+	private long createdTime;
+	private User user;
+	private String message;
+	public Message(User user, String message) {
+		super();
+		this.createdTime= System.currentTimeMillis();
+		this.user = user;
+		this.message = message;
+	}
+	@Override
+	public String toString() {
+		return  user + " : " + message;
+	}
+	
+
+}
